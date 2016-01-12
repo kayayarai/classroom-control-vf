7d66bd6edb2297439fa752c6a240a9b9197e8117
Kari Halsted <kari@puppetlabs.com>
2016-01-12 16:35:43 -0600

    first draft of init.pp for nginx module

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,29 @@
+class nginx {
+
+package { 'nginx':
+  ensure => 'present',
+}
+
+file {'/etc/nginx/':
+  ensure => 'directory',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+}
+
+file {'/etc/nginx/nginx.conf':
+  ensure => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => 'files:///modules/nginx/nginx.conf',
+  requires => Package['nginx'],
+
+}
+
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+  subscribe => File['/etc/nginx/nginx.conf'],
+
+}
