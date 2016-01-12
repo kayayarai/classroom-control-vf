97bf84ef862c4251acb0f74a6d37c58aedbd4043
Kari Halsted <kari@puppetlabs.com>
2016-01-12 15:19:41 -0600

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,5 +1,22 @@
 class memcached {
 
-  package 
+  package {'memcached':
+    ensure => 'present';
+    before => File['mem'],
   
+  }
+  
+  file {'/etc/sysconfig/memcached':
+    ensure => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/memcached/memcached',
+    }
+    
+  service {'memcached':
+    ensure => 'running',
+    enable => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+    }
   }
