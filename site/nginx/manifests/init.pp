284e351ab2fcea2d51bf966e695c49bcfc8ee266
Kari Halsted <kari@puppetlabs.com>
2016-01-12 17:09:11 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -56,8 +56,7 @@ file {'/etc/nginx/conf.d/default.conf':
 service { 'nginx':
   ensure => 'running',
   enable => true,
-  subscribe => File['/etc/nginx/nginx.conf'],
-  subscribe => File['/etc/nginx/conf.d/default.conf'],
+  subscribe => [File['/etc/nginx/nginx.conf'],File['/etc/nginx/conf.d/default.conf']],
 }
 
 
