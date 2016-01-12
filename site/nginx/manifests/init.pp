41ec415a3560608a1fb19d7c8dbee1d056b632da
Kari Halsted <kari@puppetlabs.com>
2016-01-12 16:44:38 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -11,12 +11,28 @@ file {'/etc/nginx/':
   mode   => '0755',
 }
 
+file {'/var/www':
+  ensure => 'directory',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+}
+
 file {'/etc/nginx/nginx.conf':
   ensure => file,
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
   content => 'files:///modules/nginx/nginx.conf',
+  requires => Package['nginx'],
+
+}
+file {'/var/www/index.html':
+  ensure => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => 'files:///modules/nginx/index.html',
   requires => Package['nginx'],
 
 }
