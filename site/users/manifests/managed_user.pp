2e9f7f848261371d8c6002211a4903fbafb24afa
Kari Halsted <kari@puppetlabs.com>
2016-01-13 17:29:00 -0600

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,14 @@
+define users::managed_user (
+  $user,
+  $homedir = "/${title}",
+  $group = "restricted",
+){
+file { "/${title}/welcome.txt":
+    ensure  => file,
+    owner   => $title,
+    group   => 'restricted',
+    mode    => '0644',
+    source => 'users/welcome.txt',
+
+
+}
