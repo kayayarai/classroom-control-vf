b32d73e71a4a212504befbbfac1a86713fdb0518
Kari Halsted <kari@puppetlabs.com>
2016-01-13 17:55:27 -0600

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -2,7 +2,18 @@ define users::managed_user (
   $homedir = "/${title}",
   $group = "admins",
 ){
-file { "/${title}/welcome.txt":
+user { $title:
+  ensure => present,
+  }
+  
+file {"/home/${title}":
+  ensure => directory,
+  owner => $title,
+  group => 'admins',
+  mode => '0755',
+
+}
+file { "/home/${title}/welcome.txt":
     ensure  => file,
     owner   => $title,
     group   => 'admins',
