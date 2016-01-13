750a304d154fca9be204d3240e44cee2143e043b
Kari Halsted <kari@puppetlabs.com>
2016-01-13 12:08:21 -0600

    code simplification

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,52 +1,32 @@
 class nginx {
 
+File {
+  owner => 'root',
+  group => 'root',
+  mode  => '0644',
+  }
 package { 'nginx':
   ensure => 'present',
 }
 
-file {'/etc/nginx':
+file {['/etc/nginx', '/var/www', '/etc/nginx/conf.d']:
   ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0755',
 }
 
-file {'/var/www':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0755',
-}
-
-file {'/etc/nginx/conf.d':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0755',
-}
 
 file {'/etc/nginx/nginx.conf':
   ensure => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   source => 'puppet:///modules/nginx/nginx.conf',
   require => Package['nginx'],
 
 }
 file {'/var/www/index.html':
   ensure => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   source => 'puppet:///modules/nginx/index.html',
 
 }
 file {'/etc/nginx/conf.d/default.conf':
   ensure => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   source => 'puppet:///modules/nginx/default.conf',
   require => Package['nginx'],
 
