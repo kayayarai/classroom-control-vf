e53add8d35bac771dc265b10e2bb26988b4f9c0e
Kari Halsted <kari@puppetlabs.com>
2016-01-13 18:04:27 -0600

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,12 +1,9 @@
 class users::admins {
 
 users::managed_user {'jose':
-  ensure => present,
 }
 users::managed_user {'alice':
-  ensure => present,
 }
 users::managed_user {'chen':
-  ensure => present,
 }
 }
