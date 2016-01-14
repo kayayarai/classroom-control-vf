dd13d3c5b01f6196fb8ca7a3df51080fc0a0a4e1
Kari Halsted <kari@puppetlabs.com>
2016-01-14 15:23:50 -0600

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -4,45 +4,38 @@ class nginx::params {
       $packagename = 'nginx'
       $ownername   = 'root'
       $groupname   = 'root'
-    # $docroot     = '/var/www'
+      $docroot     = '/var/www'
       $configpath  = '/etc/nginx'
       $blockpath   = '/etc/nginx/conf.d'
       $logpath     = '/var/log/nginx'
       $servicename = 'nginx'
       $serviceuser = 'nginx'
-      $default_docroot     = '/var/www'
     }
     'debian': {
       $packagename = 'nginx'
       $ownername   = 'root'
       $groupname   = 'root'
-    # $docroot     = '/var/www'
+      $docroot     = '/var/www'
       $configpath  = '/etc/nginx'
       $blockpath   = '/etc/nginx/conf.d'
       $logpath     = '/var/log/nginx'
       $servicename = 'nginx'
       $serviceuser = 'www-data'
-      $default_docroot     = '/var/www'
     }
     'windows': {
       $packagename = 'nginx-service'
       $ownername   = 'Administrator'
       $groupname   = 'Administrators'
-    # $docroot     = 'C:/ProgramData/nginx/html'
+      $docroot     = 'C:/ProgramData/nginx/html'
       $configpath  = 'C:/ProgramData/nginx'
       $blockpath   = 'C:/ProgramData/nginx/conf.d'
       $logpath     = 'C:/ProgramData/nginx/logs'
       $servicename = 'nginx'
       $serviceuser = 'nobody'
-      $default_docroot     = 'C:/ProgramData/nginx/html'
     }
     default: {
         fail("Operating system ${::osfamily} is not supported.")
     }
   }
   
-  $docroot = $root ? {
-    undef => $default_docroot,
-    default => $root,
-  }
 }
